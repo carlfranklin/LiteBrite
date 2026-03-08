79b8d0633a1c894ab5660f430f8b1d882758d5c5
Carl Franklin <carl@franklins.net>
2026-03-07 20:54:17 -0500

    Added Print function

--- LiteBrite/wwwroot/litebrite.js
+++ LiteBrite/wwwroot/litebrite.js
@@ -4,5 +4,14 @@ window.liteBrite = {
     },
     setItem: function (key, value) {
         localStorage.setItem(key, value);
+    },
+    printWithTitle: function (title) {
+        var original = document.title;
+        document.title = title;
+        window.addEventListener('afterprint', function restore() {
+            document.title = original;
+            window.removeEventListener('afterprint', restore);
+        });
+        window.print();
     }
 };
